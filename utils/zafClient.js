ebe21c01eaa7fea76a30343a983d47e361453650
Erica Girges <erica.girges@zendesk.com>
2021-08-03 20:21:36 -0700

    zaf client

---
+++ utils/zafClient.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+let zafClient = null;
+
+export function useZafClient() {
+  const [client, setClient] = useState(zafClient)
+
+  useEffect(() => {
+    // check to make sure we are not already using ZAF client OR that the client is not undefined
+    if (!client && typeof window.ZAFClient !== 'undefined') {
+      // save the zafClient to global scope so that it can be referenced without re-initialization later
+      zafClient = window.ZAFClient.init()
+
+      // set it to state
+      setClient(zafClient)
+      zafClient.invoke()
+    }
+  }, [client])
+
+  return client
+}
