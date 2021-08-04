b21aa1a3d5187ed5c3f13df084b8d9a4e83338e7
Erica Girges <erica.girges@zendesk.com>
2021-08-03 20:25:59 -0700

    script added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Script from 'next/script';
+import { useZafClient } from '../utils/zafClient';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+    // initialize the zafClient
+    useZafClient();
+
+    return (
+      <>
+        <Script
+          type='text/javascript'
+          src='https://static.zdassets.com/zendesk_app_framework_sdk/2.0/zaf_sdk.min.js'
+          strategy='beforeInteractive'
+        />
+        <Component {...pageProps} />
+      </>
+    );
+  }
 
 export default MyApp
